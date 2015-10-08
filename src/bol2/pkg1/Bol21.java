a2565a623fc2e6f4856eaf8d4a0adc584a735bfa
plagoromani <plagoromani@atanasoff09.danielcastelao.org>
2015-10-08 09:05:15 +0200

    Fin

---
+++ src/bol2/pkg1/Bol21.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol2.pkg1;
+public class Bol21 {
+
+   
+    public static void main(String[] args) {
+        float base = 2.0f ,altura = 3.0f,
+                area = base*altura/2 ;//Poño un f por ser de tipo float, senón o pon en double, tamén se pode facer un cast, valor=(tipo)expresión
+     System.out.println("area = "+ area);
+    }
+    
+}
